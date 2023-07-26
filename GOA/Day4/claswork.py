f35eb664cb60dc9d8a94c9d1bec4e8cf40fb9848
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-07-26 11:24:36 +0400

    Update claswork.py

---
+++ GOA/Day4/claswork.py
@@ -0,0 +1,21 @@
+# salari=1500
+# new_salari=100
+# new_salari2=5
+# new_salari3=(niwsalari * niwsalari2) 
+# print( salari + niwsalari3 )
+
+
+# user_name=input("whats your name: ")
+# print("man tqva rom mas qvia"+ user_name )
+# surname=input("whats your surname: ")
+# print("man tqva rom misi gvaria" + surname)
+
+# user_name=input("whats your name: ")
+# surname=input("whats your surname: ")
+# age=input("wats your age: ")
+
+# print("man tqva rom mas qvia: "+ user_name + ("man tqva rom misi gvaria" + surname) + ("man tqva rom is aris" + age))
+
+age= "10"
+new_age= int(age) + 5
+print(new_age)
