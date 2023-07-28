01991a20bf4d08f59865386378e5d906f22f5a92
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-07-28 19:32:38 +0400

    Update homework.py

---
+++ GOA/Day7/homework.py
@@ -0,0 +1,18 @@
+# zura=11
+# gabrieli=11
+# print(gabrieli + zura > 20)
+
+# GOA="99"
+# sxva_akademia="1"
+# print( GOA > sxva_akademia )
+
+lvl="akademiis leveli"
+GOA="99"
+sxva_akademia="80"
+sxva_akademia1="79"
+sxva_akademia2="53"
+sxva_akademia3="49"
+print(sxva_akademia < GOA)
+print(sxva_akademia1 < GOA)
+print(sxva_akademia2 < GOA)
+print(sxva_akademia3 < GOA)
