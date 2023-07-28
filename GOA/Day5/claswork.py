3ea7988dd6cf874029bf1afc636c062363c394a2
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-07-28 13:36:07 +0400

    Update claswork.py

---
+++ GOA/Day5/claswork.py
@@ -0,0 +1,20 @@
+#asaki = input('sheiyvanet tqveni asaki: ')
+# insaxeli = put('sheiyvanet tqveni saxeli: ')
+# asaki = input('sheiyvanet tqveni asaki: ')
+
+# print(saxeli)
+
+
+# asaki = input('sheiyvanet tqveni asaki: ')
+
+# print('chemi asaki aris ' + asaki)
+
+# name1='15'
+# name2='20'
+
+# print(name1 + name2)
+saxeli = input('tqveni saxeli - ')
+
+gvari = input('tqveni gvari - ')
+
+print('cemi saxelia' + saxeli +''+ gvari)
