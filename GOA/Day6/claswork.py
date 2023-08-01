de2ff596c2a05c784ac7326951f364f68a1a6b52
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-08-02 00:31:01 +0400

    Update claswork.py

---
+++ GOA/Day6/claswork.py
@@ -0,0 +1,36 @@
+# savings = input('ramdeni dolari gaqvs?')
+
+# savings = int(savings)
+
+# balanse = savings * 1.05
+# print(balanse)
+
+#print(11 + 5)
+
+#type(16.4)
+# distnce=14
+# units=int("16")
+# print(distnce + units)
+
+#print(14 + int("km"))
+
+#x=5.6
+#print(type(x))
+
+# age=input(" Enter your age : ")
+# print("10 wlis shemdeg iqnebi" + str(int(age) + 10) , "wlis")
+
+# print(10+10)
+# p1=10
+# p2=10
+# print(p1+p2)
+
+# print(type("14"))
+
+# olo1=int(input())
+# olo2=int(input())
+# print(olo1+olo2)
+# olo = 15 < 5
+# print(olo)
+
+print(type(5 < 9))
