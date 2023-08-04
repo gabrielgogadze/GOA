639aab86533b729083156ce776f3d2dcbda2cb09
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-08-04 18:46:13 +0400

    Update homework.py

--- GOA/Day8/homework.py
+++ GOA/Day8/homework.py
@@ -2,288 +2,126 @@
 
 # for i in range(1):
 #     print(1*1)
-
-# for i in range(1):
 #     print(1*2)
-
-# for i in range(1):
 #     print(1*3)
-
-# for i in range(1):
 #     print(1*4)
-
-# for i in range(1):
 #     print(1*5)
-
-# for i in range(1):
 #     print(1*6)
-
-# for i in range(1):
 #     print(1*7)
-
-# for i in range(1):
 #     print(1*8)
-
-# for i in range(1):
 #     print(1*9)
 
 #        x2
 
 # for i in range(1):
 #     print(2*1)
-
-# for i in range(1):
 #     print(2*2)
-
-# for i in range(1):
 #     print(2*3)
-
-# for i in range(1):
 #     print(2*4)
-
-# for i in range(1):
 #     print(2*5)
-
-# for i in range(1):
 #     print(2*6)
-
-# for i in range(1):
 #     print(2*7)
-
-# for i in range(1):
 #     print(2*8)
-
-# for i in range(1):
 #     print(2*9)
 
 #       x3
 
 # for i in range(1):
 #     print(3*1)
-
-# for i in range(1):
 #     print(3*2)
-
-# for i in range(1):
 #     print(3*3)
-
-# for i in range(1):
 #     print(3*4)
-
-# for i in range(1):
 #     print(3*5)
-
-# for i in range(1):
 #     print(3*6)
-
-# for i in range(1):
 #     print(3*7)
-
-# for i in range(1):
 #     print(3*8)
-
-# for i in range(1):
 #     print(3*9)
 
 #       x4
 
 # for i in range(1):
 #     print(4*1)
-
-# for i in range(1):
 #     print(4*2)
-
-# for i in range(1):
 #     print(4*3)
-
-# for i in range(1):
 #     print(4*4)
-
-# for i in range(1):
 #     print(4*5)
-
-# for i in range(1):
 #     print(4*6)
-
-# for i in range(1):
 #     print(4*7)
-
-# for i in range(1):
 #     print(4*8)
-
-# for i in range(1):
 #     print(4*9)
 
  #      x5
-
-# for i in range(1):
+#    or i in rangfe(1)
 #     print(5*1)
-
-# for i in range(1):
 #     print(5*2)
-
-# for i in range(1):
 #     print(5*3)
-
-# for i in range(1):
 #     print(5*4)
-
-# for i in range(1):
 #     print(5*5)
-
-# for i in range(1):
 #     print(5*6)
-
-# for i in range(1):
 #     print(5*7)
-
-# for i in range(1):
 #     print(5*8)
-
-# for i in range(1):
 #     print(5*9)
 
 #       x6
 
 # for i in range(1):
 #     print(6*1)
-
-# for i in range(1):
 #     print(6*2)
-
-# for i in range(1):
 #     print(6*3)
-
-# for i in range(1):
 #     print(6*4)
-
-# for i in range(1):
 #     print(6*5)
-
-# for i in range(1):
 #     print(6*6)
-    
-# for i in range(1):
 #     print(6*7)
-
-# for i in range(1):
 #     print(6*8)
-
-# for i in range(1):
 #     print(6*9)
 
 #       x7
 
-# for i in range(1):
 #     print(7*1)
-
-# for i in range(1):
 #     print(7*2)
-
-# for i in range(1):
 #     print(7*3)
-
-# for i in range(1):
 #     print(7*4)
-
-# for i in range(1):
 #     print(7*5)
-
-# for i in range(1):
 #     print(7*6)
-
-# for i in range(1):
 #     print(7*7)
-
-# for i in range(1):
 #     print(7*8)
-
-# for i in range(1):
 #     print(7*9)
 
 #       x8
 
 # for i in range(1):
 #     print(8*1)
-
-# for i in range(1):
 #     print(8*2)
-
-# for i in range(1):
 #     print(8*3)
-
-# for i in range(1):
 #     print(8*4)
-
-# for i in range(1):
 #     print(8*5)
-
-# for i in range(1):
 #     print(8*6)
-
-# for i in range(1):
 #     print(8*7)
-
-# for i in range(1):
 #     print(8*8)
-
-# for i in range(1):
 #     print(8*9)
 
 #        x9
 
 # for i in range(1):
 #     print(9*1)
-
-# for i in range(1):
 #     print(9*2)
-
-# for i in range(1):
 #     print(9*3)
-
-# for i in range(1):
 #     print(9*4)
-
-# for i in range(1):
 #     print(9*5)
-
-# for i in range(1):
 #     print(9*6)
-
-# for i in range(1):
 #     print(9*7)
-
-# for i in range(1):
 #     print(9*8)
-
-# for i in range(1):
 #     print(9*9)
 
 #        x10
 
 for i in range(1):
     print(10*1)
-
-for i in range(1):
     print(10*2)
-
-for i in range(1):
     print(10*3)
-
-for i in range(1):
     print(10*4)
-
-for i in range(1):
     print(10*5)
-
-for i in range(1):
     print(10*6)
-
-for i in range(1):
     print(10*7)
-
-for i in range(1):
     print(10*8)
-
-for i in range(1):
     print(10*9)
