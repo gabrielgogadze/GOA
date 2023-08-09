ec5891d002feb26cf8fdad64ad8cdfef34cc8827
gabrielgogadze <139336242+gabrielgogadze@users.noreply.github.com>
2023-08-09 08:25:55 +0400

    Add files via upload

---
+++ GOA/Day9/homework.py
@@ -0,0 +1,12 @@
+print( "your age""-8" )
+
+
+for i  in range(1):
+    print("gilocav 1-i wlis gaxdi ")
+    print("gilocav 2-i wlis gaxdi ")
+    print("gilocav 3-i wlis gaxdi ")
+    print("gilocav 4-i wlis gaxdi ")
+    print("gilocav 5-i wlis gaxdi ") 
+    print("gilocav 6-i wlis gaxdi ")
+    print("gilocav 7-i wlisgaxdi ")
+    print("gilocav shen gaxdi 8 wlis da shexvedi GOA-shi axla sheni cxovreba sheicvleba uketesobisken ")
